0c335fbb30e6c8b4a5c3427fa9e6260bbbce7694
xrhahelry <sujalbajracharya1@gmail.com>
2025-08-24 09:02:25 +0545

    signup connected to database also chat input fixed.

--- app/models.py
+++ app/models.py
@@ -7,6 +7,10 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
+    fullname = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    default_academic_level = db.Column(db.String(50), nullable=True) 
+    default_subject = db.Column(db.String(50), nullable=True)        
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
